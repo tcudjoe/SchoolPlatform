44fa789e19249474a977e05af9e145afb949f57d
Tyra Afoley Okai Cudjoe <tyra20015@gmail.com>
2025-07-02 13:52:10 +0200

    Add properties to baseemployee entity

--- edu-platform-backend/src/main/java/org/tcudjoe/eduplatformbackend/domain/shared/BaseEmployee.java
+++ edu-platform-backend/src/main/java/org/tcudjoe/eduplatformbackend/domain/shared/BaseEmployee.java
@@ -1,4 +1,19 @@
 package org.tcudjoe.eduplatformbackend.domain.shared;
 
-public class BaseEmployee {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+@Entity
+@Getter
+@Setter
+public class BaseEmployee extends BaseUser{
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long employeeNumber;
+	private LocalDate hireDate;
+	private String phonenumber;
+	private String function;
 }
