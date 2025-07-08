c552a526b481c74b4d71d491bdb50adb198a1b9d
Tyra Afoley Okai Cudjoe <tyra20015@gmail.com>
2025-07-08 22:43:13 +0200

    Add RoleEnum AND PERMISSIONS

--- edu-platform-backend/src/main/java/org/tcudjoe/eduplatformbackend/domain/shared/RoleEnum.java
+++ edu-platform-backend/src/main/java/org/tcudjoe/eduplatformbackend/domain/shared/RoleEnum.java
@@ -1,8 +1,68 @@
 package org.tcudjoe.eduplatformbackend.domain.shared;
 
-public enum RoleEnum {
-	STUDENT,
-	TEACHER,
-	PARENT,
-	ADMIN
+import org.springframework.security.core.GrantedAuthority;
+
+
+import java.util.Set;
+
+public enum RoleEnum implements GrantedAuthority {
+	STUDENT(Set.of(
+			Permission.STUDENT_READ,
+			Permission.STUDENT_WRITE,
+			Permission.STUDENT_UPDATE,
+			Permission.STUDENT_DELETE)
+	),
+	TEACHER(Set.of(
+			Permission.STUDENT_READ,
+			Permission.STUDENT_WRITE,
+			Permission.STUDENT_UPDATE,
+			Permission.STUDENT_DELETE,
+			Permission.TEACHER_READ,
+			Permission.TEACHER_WRITE,
+			Permission.TEACHER_UPDATE,
+			Permission.TEACHER_DELETE)
+	),
+	PARENT(Set.of(
+			Permission.STUDENT_READ,
+			Permission.STUDENT_WRITE,
+			Permission.STUDENT_UPDATE,
+			Permission.STUDENT_DELETE,
+			Permission.PARENT_READ,
+			Permission.PARENT_WRITE,
+			Permission.PARENT_UPDATE,
+			Permission.PARENT_DELETE)
+	),
+	ADMIN(Set.of(
+			Permission.STUDENT_READ,
+			Permission.STUDENT_WRITE,
+			Permission.STUDENT_UPDATE,
+			Permission.STUDENT_DELETE,
+			Permission.TEACHER_READ,
+			Permission.TEACHER_WRITE,
+			Permission.TEACHER_UPDATE,
+			Permission.TEACHER_DELETE,
+			Permission.PARENT_READ,
+			Permission.PARENT_WRITE,
+			Permission.PARENT_UPDATE,
+			Permission.PARENT_DELETE,
+			Permission.ADMIN_READ,
+			Permission.ADMIN_WRITE,
+			Permission.ADMIN_UPDATE,
+			Permission.ADMIN_DELETE)
+	);
+
+	private final Set<Permission> permissions;
+
+	RoleEnum(Set<Permission> permissions){
+		this.permissions = permissions;
+	}
+
+	public Set<Permission> getPermissions() {
+		return permissions;
+	}
+
+	@Override
+	public String getAuthority(){
+		return "ROLE_" + this.name();
+	}
 }
