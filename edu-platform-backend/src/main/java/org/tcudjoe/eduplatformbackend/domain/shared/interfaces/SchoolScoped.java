7ee40447536723df491b628de86ec8daad5c1c7f
Tyra Afoley Okai Cudjoe <tyra20015@gmail.com>
2025-07-12 22:10:59 +0200

    Add schoolscoped interface

---
+++ edu-platform-backend/src/main/java/org/tcudjoe/eduplatformbackend/domain/shared/interfaces/SchoolScoped.java
@@ -0,0 +1,7 @@
+package org.tcudjoe.eduplatformbackend.domain.shared.interfaces;
+
+import org.tcudjoe.eduplatformbackend.domain.school.School;
+
+public interface SchoolScoped {
+	School getSchool();
+}
